f8e7667a7e0992823fafc4d91347224e4e1777b1
CyberRootkie <126771070+CyberRootkie@users.noreply.github.com>
2023-03-04 10:03:26 +0100

    Gestion du verbose

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ import argparse
 # On définit les arguments nécessaires, les valeurs par défaut et l'aide
 parser = argparse.ArgumentParser()
 parser.add_argument("--name", type=str, help="Name of the person", default="JB")
-parser.add_argument("--name2", type=str, help="Name of the person 2", default="JB2")
+parser.add_argument("--name2", type=str, help="Name of the person 2", required=True)
+parser.add_argument('--verbose', '-v', action='count', default=0)
 
 # On récupère les arguments
 args = parser.parse_args()
 
 # On les affiche
 print(f"coucou {args.name} et {args.name2}")
+
+if args.verbose:
+    print('Verbose activé')
+else:
+    print('Verbose NON activé')
